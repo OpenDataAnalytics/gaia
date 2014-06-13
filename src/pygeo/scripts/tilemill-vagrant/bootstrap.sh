55b4cc315f86a8783d684a4eb846cfa76ae16887
Chris Harris <chris.harris@kitware.com>
2014-06-13 09:53:06 -0400

    Fix up bootstrap.sh to work in vagrant

--- src/pygeo/scripts/tilemill-vagrant/bootstrap.sh
+++ src/pygeo/scripts/tilemill-vagrant/bootstrap.sh
@@ -1,56 +1,60 @@
 #!/usr/bin/env bash
 
-# First, clear out any old mapnik or node.js installs that might conflict
-sudo apt-get purge libmapnik libmapnik-dev mapnik-utils nodejs
-
 # Also clear out any old ppa's that might conflict
-sudo rm /etc/apt/sources.list.d/*mapnik*
-sudo rm /etc/apt/sources.list.d/*developmentseed*
-sudo rm /etc/apt/sources.list.d/*chris-lea*
+rm -f /etc/apt/sources.list.d/*mapnik*
+rm -f /etc/apt/sources.list.d/*developmentseed*
+rm -f /etc/apt/sources.list.d/*chris-lea*
+
+export DEBIAN_FRONTEND=noninteractive
+# update
+apt-get -y update
+apt-get -y upgrade
+
+# Install some utilities 
+apt-get install -y software-properties-common python-software-properties
 
 # add new ppa's
-echo 'yes' | sudo apt-add-repository ppa:chris-lea/node.js
-echo 'yes' | sudo apt-add-repository ppa:mapnik/v2.2.0
+apt-add-repository -y ppa:chris-lea/node.js
+apt-add-repository -y ppa:mapnik/v2.2.0
 
-# update
-sudo apt-get -y update
-sudo apt-get -y upgrade
+apt-get -y update
+
+# First, clear out any old mapnik or node.js installs that might conflict
+apt-get purge -qq libmapnik libmapnik-dev mapnik-utils nodejs
+
 
 # install nodejs latest and a few tilemill deps
-sudo apt-get install -y nodejs git build-essential libgtk2.0-dev \
- libwebkitgtk-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev \
- libgdal1-dev
+apt-get install -y nodejs git build-essential libgtk2.0-dev \
+libwebkitgtk-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev \
+libgdal1-dev
 
 
 # Now, either install mapnik latest from packages
 # Or see file below for installing mapnik from source
 # and skip this line
-sudo apt-get install -y libmapnik-dev mapnik-utils
+apt-get install -y libmapnik-dev mapnik-utils
 
 # set up postgres
-POSTGRES_VERSION=9.1 # you may need to change this depending on ubuntu version
-POSTGIS_VERSION="1.5" # you may need to change this depending on ubuntu version
-sudo apt-get install -y postgresql postgresql-server-dev-$POSTGRES_VERSION postgresql-$POSTGRES_VERSION-postgis
-sudo su postgres
-# we lost variables, reset them
-POSTGRES_VERSION=9.1 # you may need to change this
-POSTGIS_VERSION="1.5" # you may need to change this
-createuser <your user> # yes to super
-createdb template_postgis
-createlang -d template_postgis plpgsql # you may not need this
-POSTGIS_PATH=`pg_config --sharedir`/contrib/postgis-$POSTGIS_VERSION
-psql -d template_postgis -f $POSTGIS_PATH/postgis.sql
-psql -d template_postgis -f $POSTGIS_PATH/spatial_ref_sys.sql
-exit
+export POSTGRES_VERSION=9.1 # you may need to change this depending on ubuntu version
+export POSTGIS_VERSION="1.5" # you may need to change this depending on ubuntu version
+apt-get install -y postgresql postgresql-server-dev-$POSTGRES_VERSION postgresql-$POSTGRES_VERSION-postgis
+#createuser <your user> # yes to super
+sudo -E -u postgres createdb template_postgis
+sudo -E -u postgres createlang -d template_postgis plpgsql # you may not need this
+export POSTGIS_PATH=`pg_config --sharedir`/contrib/postgis-$POSTGIS_VERSION
+sudo -E -u postgres psql -d template_postgis -f $POSTGIS_PATH/postgis.sql
+sudo -E -u postgres psql -d template_postgis -f $POSTGIS_PATH/spatial_ref_sys.sql
 
 # build tilemill
-git clone https://github.com/mapbox/tilemill.git
-cd tilemill
+
+sudo -u vagrant git clone https://github.com/mapbox/tilemill.git /home/vagrant/tilemill
+cd /home/vagrant/tilemill
 npm install
 
 # then start it...
 # if you are running a desktop server then just boot using all the defaults
-./index.js # should open a window automatically, but you can also view at http://localhost:20009
+#/home/vagrant/tilemill/index.js tile
+#/home/vagrant/tilemill/index.js core --listenHost=0.0.0.0 # should open a window automatically, but you can also view at http://localhost:20009
 
 # if you are running a headless/remote server then you can connect either
 # by ssh connection forwarding or by opening up public access to the machine.
