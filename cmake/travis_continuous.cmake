5bef19e6b1a0d3047e57a9c048a62f1c4e41c351
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-02-20 09:16:01 -0500

    Put python version number in build name

--- cmake/travis_continuous.cmake
+++ cmake/travis_continuous.cmake
@@ -3,7 +3,7 @@ set(CTEST_BINARY_DIRECTORY "$ENV{TRAVIS_BUILD_DIR}/_build")
 
 include(${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake)
 set(CTEST_SITE "Travis")
-set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}")
+set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}-$ENV{CMAKE_PYTHON_VERSION}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 ctest_start("Continuous")
