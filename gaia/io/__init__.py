de639d109304bf5fe56e5bfe3ebfe3a4001424d8
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-04-05 17:43:15 -0400

    Initial import of netcdf reader

---
+++ gaia/io/__init__.py
@@ -0,0 +1,13 @@
+"""This package contains interface adapters for IO.
+
+"""
+
+__all__ = ()
+
+try:
+    import netCDF4
+except ImportError:
+    netCDF4 = None
+
+if netCDF4 is not None:
+    __all__ += ('NetCDFReader',)
---
+++ gaia/io/netcdf_reader.py
@@ -0,0 +1,45 @@
+"""This module defines a reader based on the netcdf."""
+
+from gaia.core import Task
+from gaia.core import data
+
+import netCDF4 as nc
+
+import os
+
+class NetCDFReader(Task):
+
+    """A task that reads netcdf files using netCDF4."""
+
+    output_ports = [
+        data.Data.make_output_port()
+    ]
+
+    def _reset(self, *args):
+        """Remove data cache."""
+        self.dirty = True
+        self._output_data[''] = None
+
+    def run(self, *args, **kw):
+        """Read and cache file data using netCDF4."""
+
+        import numpy as np
+        import json
+
+        super(NetCDFReader, self).run(*args, **kw)
+
+        # Currently we are ignoring the metadata but that could
+        # fixed in later revisions
+        if self._output_data.get('') is None:
+            result = {}
+            f = nc.Dataset(self.file_name)
+            for key in f.variables.keys():
+                newvar = f.variables[key]
+                result[key] = np.array(newvar[:]).tolist()
+            self._output_data[''] = json.dumps(result)
+
+NetCDFReader.add_property(
+    'file_name',
+    on_change=NetCDFReader._reset,
+    validator=os.path.exists
+)
