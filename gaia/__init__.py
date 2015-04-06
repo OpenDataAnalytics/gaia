f3bf35c4fb779928807b738259ee1f8ef91d6f90
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-04-06 16:16:36 -0400

    Adding a global configuration object

--- gaia/__init__.py
+++ gaia/__init__.py
@@ -1,2 +1,6 @@
+from gaia._config import config
+
 #: Global version number for the package
 version = '0.0.1-alpha'
+
+__all__ = ('version', 'config')
---
+++ gaia/_config.py
@@ -0,0 +1,22 @@
+"""This module contains the global configuration for gaia.
+
+Submodules and plugins can store any user/site configuration data
+in this object (in a unique section).
+"""
+
+from six.moves import configparser
+from collections import OrderedDict
+
+ConfigParser = configparser.ConfigParser
+if hasattr(configparser, 'SafeConfigParser'):  # pragma: nocover
+    ConfigParser = configparser.SafeConfigParser
+
+kw = {
+    'dict_type': OrderedDict
+}
+if hasattr(configparser, 'ExtendedInterpolation'):  # pragma: nocover
+    kw['interpolation'] = configparser.ExtendedInterpolation()
+
+config = ConfigParser(**kw)
+
+__all__ = ('config',)
