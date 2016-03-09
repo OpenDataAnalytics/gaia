202d47f4e8c3c8514e01bd9c1fdde7d17a6e24e6
Matt Bertrand <matt@epidemico.com>
2016-03-09 17:57:49 -0500

    Geoserver proxy

--- gaia/tasks.py
+++ gaia/tasks.py
@@ -3,8 +3,8 @@ from gaia.parser import parse_request
 from gaia.core import config
 
 app = Celery('tasks',
-             backend=config['gaia']['celery_backend'],
-             broker=config['gaia']['celery_broker'])
+             backend=config['gaia_celery']['celery_backend'],
+             broker=config['gaia_celery']['celery_broker'])
 
 
 @app.task
