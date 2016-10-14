d31d526ca6c2580efad6c9698fd831f2556a2e51
Matt Bertrand <matt@epidemico.com>
2016-10-14 12:44:29 -0400

    Remove girder plugin code (moved to gaia_minerva repo)

--- plugin.cmake
+++
@@ -1,4 +0,0 @@
-add_python_test(geoprocess PLUGIN gaia)
-add_python_test(geoservice_proxy PLUGIN gaia)
-add_python_style_test(pep8_style_gaia_rest
-                      "${PROJECT_SOURCE_DIR}/plugins/gaia/server/rest")
