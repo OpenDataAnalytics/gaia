307220215de71fa65944eaa3bfea5f7c49e468c8
john Tourtellott <john.tourtellott@kitware.com>
2018-08-06 10:08:41 -0400

    Add a minimal gaia.show() function
    * Only works with geojson data

--- gaia/__init__.py
+++ gaia/__init__.py
@@ -118,3 +118,64 @@ def get_plugins():
 
 
 get_config()
+
+def show(*data_objects):
+    """
+    Displays data objects using available rendering code
+
+    :param data_object: 1 or more GeoData objects
+    """
+    if not data_objects:
+        print('(no data objects)')
+        return
+
+    # Is jupyterlab_geojs available?
+    try:
+        import json
+        from IPython.display import display
+        import jupyterlab_geojs
+    except ImportError as err:
+        print(data_objects)
+        return
+
+    # (else)
+    #print(data_objects)
+    scene = jupyterlab_geojs.Scene()
+    scene.create_layer('osm')
+    feature_layer = scene.create_layer('feature')
+
+    combined_bounds = None
+    # Reverse order so that first item ends on top
+    for data_object in reversed(data_objects):
+        # Create map feature
+        #print(data_object._getdatatype(), data_object._getdataformat())
+        # type is vector, format is [.json, .geojson, .shp, pandas]
+        data = data_object.get_data()
+
+        # Can only seem to get json *string*; so parse into json *object*
+        json_string = data.to_json()
+        json_object = json.loads(json_string)
+        feature = feature_layer.create_feature('geojson', json_object)
+        #print(json_object)
+        feature.enableToolTip = True  # dont work
+
+        geometry = data['geometry']
+        bounds = geometry.total_bounds
+        #print(bounds)
+        if combined_bounds is None:
+            combined_bounds = bounds
+        else:
+            combined_bounds[0] = min(combined_bounds[0], bounds[0])
+            combined_bounds[1] = min(combined_bounds[1], bounds[1])
+            combined_bounds[2] = max(combined_bounds[2], bounds[2])
+            combined_bounds[3] = max(combined_bounds[3], bounds[3])
+
+    #print(combined_bounds)
+    corners = [
+        [combined_bounds[0], combined_bounds[1]],
+        [combined_bounds[2], combined_bounds[1]],
+        [combined_bounds[2], combined_bounds[3]],
+        [combined_bounds[0], combined_bounds[3]]
+    ]
+    scene.set_zoom_and_center(corners=corners)
+    display(scene)
